4f0ac97524071117115839c68d59efa6a4574c88
Artem <artem-lebeedev-2004@inbox.ru>
2020-05-31 12:57:56 +0400

    создание объекта personalMovieDB

--- Practic1.js
+++ Practic1.js
@@ -1,1 +1,9 @@
 let numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actor: {},
+    genres: [],
+    private: false
+};
